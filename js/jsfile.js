5eadb7e5c7916ed8292857ece19be1692efa1dd8
dawn.cundangan <dawncundangan@gmail.com>
2019-09-12 15:45:14 +0800

    Reorganize files

--- js/jsfile.js
+++ js/jsfile.js
@@ -350,7 +350,7 @@ window.onload = function() {
   const data = getData();
   if(data!=null){
     console.log(data);
-    var $table  =  $('<table class="table table-striped table-dark rounded"><thead><tr><th scope="col">Movie Title</th><th scope="col">Cinema Number</th><th scope="col">Date</th><th scope="col">Time</th><th scope="col">Seat No.</th><th scope="col">Action</th></tr></thead></table>');
+    var $table  =  $('<table class="table table-striped table-sm compact table-dark rounded" id="reserves"><thead><tr><th scope="col">Movie Title</th><th scope="col">Cinema Number</th><th scope="col">Date</th><th scope="col">Time</th><th scope="col">Seat No.</th><th scope="col">Action</th></tr></thead></table>');
     var btn = document.createElement("BUTTON");
       btn.innerHTML = "Cancel";
       btn.setAttribute("class", "button3");
