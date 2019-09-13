50aa898169d56eec9a1565a691ceabd7ed57ba67
dawn.cundangan <dawncundangan@gmail.com>
2019-09-13 16:46:11 +0800

    Final

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	var cinamaWithMovie = [];
 	var cinamaWithoutMovie = [];
 	nowShowing();
-	$("#moviesShowing").html(htmlString);
 
 	function nowShowing() {
 		for(var movie = 0; movie < movies.length; movie++) {
@@ -28,6 +27,7 @@ $(document).ready(function() {
 	        	}
 	        }
 	    } 
+		$("#moviesShowing").html(htmlString);
 	}
 
 	/*var htmlString = "";
@@ -48,7 +48,6 @@ $(document).ready(function() {
 
     function showMovie(movie) {
     	if(cinamaWithMovie.includes(movie) == false) {
-			console.log(cinamaWithMovie);
 
 	    	htmlString += "<div class='card col-md-2 col-sm-6 px-1 my-1 homepage-movie-cards'>";
 	        htmlString += "<img src='" + movies[movie].image + "' class='card-img'>";
@@ -66,28 +65,20 @@ $(document).ready(function() {
     }
 
     function showingSoon() {
+    	var htmlString = "";
 		for(var movie = 0; movie < movies.length; movie++) {
-	        for(var cinema = 0; cinema < movies[movie].cinema_1.length; cinema++) {
-	        	if(movies[movie].cinema_1[cinema] == moment(new Date()).format("MMM D")) {
-	        		showMovie(movie);
-	        	}
-	        }
-	        for(var cinema = 0; cinema < movies[movie].cinema_2.length; cinema++) {
-	        	if(movies[movie].cinema_2[cinema] == moment(new Date()).format("MMM D")) {
-	        		showMovie(movie);
-	        	}
-	        }
-	        for(var cinema = 0; cinema < movies[movie].cinema_3.length; cinema++) {
-	        	if(movies[movie].cinema_3[cinema] == moment(new Date()).format("MMM D")) {
-	        		showMovie(movie);
-	        	}
-	        }
-	        for(var cinema = 0; cinema < movies[movie].cinema_4.length; cinema++) {
-	        	if(movies[movie].cinema_4[cinema] == moment(new Date()).format("MMM D")) {
-	        		showMovie(movie);
-	        	}
+	        if(cinamaWithMovie.includes(movie) == false) {
+	        	htmlString += "<div class='card col-md-2 col-sm-6 px-1 my-1 homepage-movie-cards'>";
+		        htmlString += "<img src='" + movies[movie].image + "' class='card-img'>";
+		        htmlString += "<div class='card-img-overlay text-center movies-now-showing'>";
+		        htmlString += "<div class='pt-sm-2 pt-m-1'><h4>" + movies[movie].name + "</h4><p class='showing-datetime mb-0'>Now Showing</p>"; 
+		        htmlString += "<p class='showing-datetime my-0'>" + movies[movie].time + "</p>";
+		        htmlString += "<span class='badge badge-warning mb-sm-2 mb-md-1'>" + movies[movie].badge + "</span></div><div class='pt-sm-5 pt-md-3'>";
+		        htmlString += "<button class='btn btn-sm button button5' data-toggle='modal' data-target='#reserve1' ";
+		        htmlString += "onclick='showDetails(" + movie + ")'>Buy Ticket</button></div></div></div>";
 	        }
 	    } 
+		$("#moviesShowing").html(htmlString);
 	}
 
 	$(".cinema-showing").on("click", function() {
